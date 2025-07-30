cae45aa4352971e66e1847f05a6d250039d508cf
DineshD5701 <dineshd3463@gmail.com>
2025-07-30 12:29:58 +0530

    edited index.js

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const PORT = process.env.PORT || 4000;
 http.createServer((req, res) => {
-  res.end("Hello from service on port " + PORT);
+  res.end("Hello from service auth-service deployed using jenkins on port " + PORT);
 }).listen(PORT, () => console.log(`Running on port ${PORT}`));
--- order-service/index.js
+++ order-service/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const PORT = process.env.PORT || 6000;
 http.createServer((req, res) => {
-  res.end("Hello from service on port " + PORT);
+  res.end("Hello from order-service deployed by jenkins ci/cd on port " + PORT);
 }).listen(PORT, () => console.log(`Running on port ${PORT}`));
