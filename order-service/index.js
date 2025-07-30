7837dd70c1aaf379669d81b09690d006ce7c0c96
DineshD5701 <dineshd3463@gmail.com>
2025-07-30 12:55:14 +0530

    changed port on order service

--- order-service/index.js
+++ order-service/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 http.createServer((req, res) => {
   res.end("Hello from order-service deployed by jenkins ci/cd on port " + PORT);
 }).listen(PORT, () => console.log(`Running on port ${PORT}`));
